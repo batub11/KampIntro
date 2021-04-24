b35b8afeb20f21f9d839f5a7048f63e719749d16
Batuhan Bayraktar <batuhan@bayraktargrup.com>
2021-04-24 04:01:13 +0300

    24.04.2021 Ders Sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    { //​(C)reate (R)ead (U)pdate (D)elete - SQL Sorgu İşlemleri
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +"Eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Güncellendi.");
+        }
+
+
+
+        
+
+    }
+}
